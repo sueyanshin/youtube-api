6c34887061de14d085c0471340bb23ccfa54e588
Sue Yan Shin <66193892+sueyanshin@users.noreply.github.com>
2025-10-15 17:57:02 +0630

    removed module

--- server.js
+++ server.js
@@ -144,4 +144,4 @@ app.get("/video/search", async (req, res) => {
 // const PORT = process.env.PORT || 3000;
 // app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
 
-module.exports = app;
+export default app;
